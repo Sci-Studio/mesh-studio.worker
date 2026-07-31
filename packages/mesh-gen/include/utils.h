ced74a98c3afac98b52f0a244ba6154ae08973ed
HishamSaeed <hisham.saeed.92@gmail.com>
2026-07-31 17:36:06 +0200

    implemented bounding box util

--- packages/mesh-gen/include/utils.h
+++ packages/mesh-gen/include/utils.h
@@ -5,4 +5,11 @@
 #include <utility>
 #include <vector>
 
+struct BBox {
+    double min_x, max_x;
+    double min_y, max_y;
+};
+
+BBox bounding_box(const std::vector<Vec2>& points);
+
 std::pair<double, double> point_minmax(const std::vector<Vec2>& points);
